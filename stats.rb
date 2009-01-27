d9d2972a783ea4b3bafb30f8f81a7c391ac96bcd
unknown <jimmysch@.redmond.corp.microsoft.com>
2009-01-27 03:37:12 -0800

    mspec stats

--- stats.rb
+++ stats.rb
@@ -57,8 +57,9 @@ module Stats
   include Helpers
   
   def build
+    FileUtils.rm "#{CD}/compile.log"
     Benchmark.measure do
-      FileUtils.cd(RB) { system 'rake compile > nul' }
+      FileUtils.cd(RB) { system "rake compile > #{CD}/compile.log" }
     end
   end
 
@@ -91,15 +92,29 @@ module Stats
     results.to_f
   end
   
-  def mspec(type)
-    unless [:core, :lang, :lib].include?(type)
+  def mspec(type = nil)
+    types = [:core, :lang, :lib]
+    types.each {|t| FileUtils.rm "#{CD}/mspec_#{t}.log" if File.exist? "#{CD}/mspec_#{t}.log" }
+    
+    type ||= :core
+    unless types.include?(type)
       puts "\"#{type}\" is not a valid mspec option"
       exit
     end
+    
+    results = nil
     FileUtils.cd(RB) do
-      results = `rake mspec:#{type}`
+      print "Running mspec:#{type} ..."
+      system "rake mspec:#{type} > #{CD}/mspec_#{type}.log"
     end
     
+    File.open("#{CD}/mspec_#{type}.log", "r") { |f| f.read }
+  end
+end
+
+class SSS
+  class << self
+    include Stats
   end
 end
 
@@ -136,6 +151,18 @@ class Report
       puts "Throughput: (100000 iterations) #{throughput.round_to(2)} seconds"
     end
 
+    def report_mspec_core
+      dmr pmr(mspec(:core))
+    end
+    
+    def report_mspec_lang
+      dmr pmr(mspec(:lang))
+    end
+    
+    def report_mspec_lib
+      dmr pmr(mspec(:lib))
+    end
+
     def report_all
       reports.each {|m| send(m) if reports.include?(m)}
     end
@@ -146,6 +173,26 @@ class Report
         list[0], list[i] = list[i], list[0]
       end
       list
+    end
+    
+  private
+    # Display Parsed MSpec Results 
+    def dmr(results)
+      results.each do |k,v| puts "#{k}:\t#{v}" end
+    end
+  
+    # Parse MSpec Results 
+    def pmr(results)
+      data = {}
+      parser = /Finished in (.*? seconds)\n\n(.*? files), (.*? examples), (.*? expectations), (.*? failures), (.*? errors)/
+      results.scan(parser) do |parsed|
+        parsed.each do |node|
+          puts node.inspect
+          s = node.split(' ')
+          data[s.last.to_sym] = s.first
+        end
+      end
+      data
     end
   end
   
@@ -158,7 +205,8 @@ end
 $behavior = {
   ['--help', '-h'] => lambda { puts usage; exit },
   ['--all']        => lambda { Report.run :all },
-  ['--console']    => lambda { puts 'Running in console mode' }
+  ['--console']    => lambda { puts 'Running in console mode' },
+  ['--clean']      => lambda { clean }
 }.merge(
   # generate a ['--#{name}'] => lambda { Report.run name } 
   # for each report in Report.reports
@@ -169,8 +217,16 @@ $behavior = {
   end
 )
 
+def clean
+  print 'removing log files ... '
+  %W(compile.log mspec_core.log mspec_lang.log mspec_lib.log).each do |f|
+    FileUtils.rm "#{CD}/#{f}" if File.exist? "#{CD}/#{f}"
+  end
+  puts 'done'
+end
+
 def usage
-  o = "usage:\n  ruby #{__FILE__}"
+  o = "usage:\n  ruby #{__FILE__[2..-1]}"
   $behavior.map{|opts, _| ' [' + opts.join('|') + ']'}.each{|i| o << i}
   o
 end
