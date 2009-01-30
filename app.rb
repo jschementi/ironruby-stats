90965bccb958360aea0294d8f2cd370867ceac54
Jimmy Schementi <jschementi@gmail.com>
2009-01-29 23:22:11 -0800

    formatting

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 
+require 'mymath'
+
+require 'activesupport'
+
 def dbg
   require 'ruby-debug'
   Debugger.start
@@ -9,15 +13,28 @@ end
 
 helpers do
   def time(t)
-    t ? "#{t} s" : "No data"
+    if t
+      t = t.to_f
+      t < 60 ? "#{t.round_to(2)} s" : "#{(t / 60).round_to(2)} m"
+    else
+      "No data"
+    end
   end
   
   def size(s)
-    s ? "#{s} mb" : "No data"
+    s ? "#{mb(s)} mb" : "No data"
   end
   
   def data(d)
     d ? d.to_s : "No data"
+  end
+  
+  def total_binary_size(binaries)
+    binaries.inject(0){|x,(_,y)| x += y }
+  end
+
+  def mb(bytes)
+    bytes./(1_000_000.0).round_to(2)
   end
 end
 
@@ -51,50 +68,59 @@ __END__
 %h1 IronRuby Stats
 
 %div
-  ir.exe -X:Interpret
+  %a{:href => 'http://github.com/jschementi/ironruby-stats'} generated
+  daily from
+  %a{:href => 'http://github.com/ironruby/ironruby'} IronRuby
 
 %table
   %thead
     %tr
-      %th{:colspan => 2} Performance
+      %th{:colspan => 3}
+        Performance
+    %tr.sub
+      %th ir.exe
+      %th default
+      %th -X:Interpret
   %tbody
     %tr
       %th Startup time
-      %td= time stats[:startup]
+      %td= time(stats[:startup][:compiled])
+      %td= time(stats[:startup][:interpreted])
     %tr
-      %th Throughput (100000 iters)
-      %td= time stats[:throughput]
+      %th 100000 iters
+      %td= time(stats[:throughput][:compiled])
+      %td= time(stats[:throughput][:interpreted])
 
   %thead
     %tr
-      %th{:colspan => 2} RubySpec
+      %th{:colspan => 3} RubySpec
   = haml :mspec, :locals => {:title => "Language", :mspec => stats[:mspec_language]}, :layout => false
   = haml :mspec, :locals => {:title => "Core", :mspec => stats[:mspec_core]}, :layout => false
   = haml :mspec, :locals => {:title => "Library", :mspec => stats[:mspec_library]}, :layout => false
 
   %thead
     %tr
-      %th{:colspan => 2} Source Code
+      %th{:colspan => 3} Source Code
   %tbody
     %tr
       %th Github repository size
-      %td= size stats[:repo]
+      %td{:colspan => 2}= size stats[:repo]
       
   %thead
     %tr
-      %th{:colspan => 2} Binaries
+      %th{:colspan => 3} Binaries
   %tbody
     %tr
       %th Build time
-      %td= time stats[:build]
+      %td{:colspan => 2}= time stats[:build]
     %tr
       %th Binary size
-      %td= size stats[:binsize]
+      %td{:colspan => 2}= size(total_binary_size(stats[:binsize]))
 
 @@ mspec
 %thead
   %tr.sub
-    %th{:colspan => 2}= title
+    %th{:colspan => 3}= title
   %tbody
     - if mspec.empty? || mspec.select{|_,v| v.to_f != 0}.empty?
       %tr
@@ -103,23 +129,23 @@ __END__
     - else
       %tr
         %th time
-        %td= time mspec[:seconds]
+        %td{:colspan => 2}= time mspec[:seconds]
       %tr
         %th files
-        %td= data mspec[:files]
+        %td{:colspan => 2}= data mspec[:files]
       %tr 
         %th examples
-        %td= data mspec[:examples]
+        %td{:colspan => 2}= data mspec[:examples]
       %tr
         %th expectations
-        %td= data mspec[:expectations]
+        %td{:colspan => 2}= data mspec[:expectations]
       %tr
         %th failures
-        %td{ :class => (mspec[:failures].to_i > 0 ? 'fail' : 'pass') }
+        %td{:colspan => 2, :class => (mspec[:failures].to_i > 0 ? 'fail' : 'pass') }
           = data mspec[:failures]
       %tr
         %th errors
-        %td{ :class => (mspec[:errors].to_i > 0 ? 'fail' : 'pass') }
+        %td{:colspan => 2, :class => (mspec[:errors].to_i > 0 ? 'fail' : 'pass') }
           = data mspec[:errors]
 
 @@ stylesheet
@@ -131,7 +157,6 @@ body
   :text-align center
 h1
   :border-bottom 2px solid #333
-
 table
   :margin-left auto
   :margin-right auto
@@ -155,6 +180,8 @@ table
       
     th, td
       :padding 5px
+    td
+      :text-align right
 .fail
   :color red
 .pass
@@ -164,3 +191,11 @@ div
   :margin-left auto
   :margin-right auto
   :position relative
+a
+  :color white
+  :padding 3px
+  :text-decoration none
+  &:link, &:visited
+    :background-color #222
+  &:hover
+    :background-color #555
--- mymath.rb
+++ mymath.rb
@@ -11,4 +11,3 @@ class Float
     (self * 10**x).floor.to_f / 10**x
   end
 end
-
